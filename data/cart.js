7a93f02557ebe43730e750287bd78b415cea2493
ArshaMeenu <arsha@sayonetech.com>
2023-11-07 15:36:29 +0530

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,18 @@
-export let cart = [{
-    productId : '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity  : 2,
+export let cart = JSON.parse(localStorage.getItem('cart'))
+ if(!cart){
+     cart = [{
+        productId : '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+        quantity  : 2,
+    },
+    {
+        productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity  : 3
+    }];
+ }
 
-},
-{
-    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity  : 3
-},
-{
-    productId : '54e0eccd-8f36-462b-b68a-8182611d9add',
-    quantity  : 1
-},];
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
  // adds the product to the cart
 export  function addToCart(productId){
              let matchingItem;
@@ -27,6 +29,7 @@ export  function addToCart(productId){
                  quantity: 1
                });
              }
+             saveToStorage();
        }
 
 export function removeFromCart(productId){
@@ -37,4 +40,5 @@ export function removeFromCart(productId){
                 }
         });
         cart = newCart;
+        saveToStorage();
 }
